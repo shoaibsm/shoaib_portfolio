fc9a2f00ef941dc97167287fe034204b8ac455cf
shoaibsm <shoaib.sm6655@gmail.com>
2024-01-14 22:21:06 +0530

    Navbar.js changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -24,49 +24,47 @@ function Navbar() {
 
     function toggler() {
         setIsNavOpen(!isNavOpen)
-        console.log(isNavOpen);
     }
 
     return (
-        <nav className='Navbar'>
-            <div className="Navbar__container" ref={navRef}>
-                <div className="Navbar__brandSection">
-                    <div className="Navbar__brandBox">
+        <div className='Navbar'>
+            <div className="container" ref={navRef}>
+                <div className="brand-section">
+                    <div className="brand-name">
                         <Link to="introduction" smooth={true} duration={500}>
                             <h2 className='name'><span>Shoaib</span> Mohammed</h2>
                         </Link>
                     </div>
                 </div>
 
-                {window.innerWidth <= 576 ? (<div className={isNavOpen ? 'Navbar__navigationSection--open' : 'Navbar__navigationSection--close'}>
+                {window.innerWidth <= 576 ? (<div className={isNavOpen ? 'navigation-section' : 'navigation-section-close'}>
                     <div className="navigation-container">
-                        <ul className='Navbar__list'>
-                            <li className='Navbar__item'><Link to="about" smooth={true} duration={500}>About Me</Link></li>
-                            <li className='Navbar__item'><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
-                            <li className='Navbar__item'><Link to="experience" smooth={true} duration={500}>Experience</Link></li>
-                            <li className='Navbar__item'><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
-                            <li className='Navbar__item'><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
+                        <ul>
+                            <li><Link to="about" smooth={true} duration={500}>About Me</Link></li>
+                            <li><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
+                            <li><Link to="experience" smooth={true} duration={500}>Experience</Link></li>
+                            <li><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
+                            <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
                         </ul>
                     </div>
-                    <div className="Navbar__btn--browse">
-                        <a className="Navbar__btn--Link" href="https://shoaib-sm-resume.onrender.com/" target="_blank" rel="noopener noreferrer">
-                            <p className='Navbar__btnText'>Browse CV</p>
-                        </a>
+                    <div className="cv-btn">
+                        <p className='btn-tex'>Download CV</p>
                     </div>
-                </div>) : (<div className={'Navbar__navigationSection--open'}>
-                    <div className="Navbar__navigationContainer">
-                        <ul className='Navbar__list'>
-                            <li className='Navbar__item'><Link to="about" smooth={true} duration={500}>About Me</Link></li>
-                            <li className='Navbar__item'><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
-                            <li className='Navbar__item'><Link to="experience" smooth={true} duration={500}>Experience</Link></li>
-                            <li className='Navbar__item'><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
-                            <li className='Navbar__item'><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
+                </div>) : (<div className={'navigation-section'}>
+                    <div className="navigation-container">
+                        <ul>
+                            <li><Link to="about" smooth={true} duration={500}>About Me</Link></li>
+                            <li><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
+                            <li><Link to="experience" smooth={true} duration={500}>Experience</Link></li>
+                            <li><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
+                            <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
                         </ul>
                     </div>
-                    <div className="Navbar__btn--browse">
-                        <a className="Navbar__btn--Link" href="https://shoaib-sm-resume.onrender.com/" target="_blank" rel="noopener noreferrer">
-                            <p className='Navbar__btnText'>Browse CV</p>
+                    <div className="cv-btn">
+                        <a href="https://shoaib-sm-resume.onrender.com/" target="_blank" rel="noopener noreferrer">
+                            <p className='btn-text'>Download CV</p>
                         </a>
+
                     </div>
                 </div>)}
 
@@ -74,7 +72,7 @@ function Navbar() {
                     <RxHamburgerMenu />
                 </div>
             </div>
-        </nav>
+        </div>
     )
 }
 
